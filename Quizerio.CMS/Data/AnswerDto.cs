32661021d7c918af55ef6e56faaa8740bb8e909e
Olka705 <aleksandrawajda704@gmail.com>
2025-06-08 16:55:39 +0200

    Added all Question endpoint pegging

--- Quizerio.CMS/Data/AnswerDto.cs
+++ Quizerio.CMS/Data/AnswerDto.cs
@@ -4,5 +4,7 @@
     {
         public string Text { get; set; } = string.Empty;
         public bool IsCorrect { get; set; } = false;
+
+        public Guid Id { get; set; } = Guid.Empty;
     }
 }
--- Quizerio.CMS/Data/QuestionDto.cs
+++ Quizerio.CMS/Data/QuestionDto.cs
@@ -1,20 +1,29 @@
 namespace Quizerio.CMS.Data
 {
-    public class QuestionDto
+    public class QuestionWriteDto
     {
-        public Guid Id { get; set; }
-
         public string QuestionText { get; set; } = string.Empty;
 
         public string Difficulty { get; set; } = string.Empty;
-
-        public string Status { get; set; } = string.Empty;
 
         public AnswerDto[] Answers { get; set; } = new AnswerDto[4]
         {
             new(), new(), new(), new()
         };
-
         public Guid CategoryId { get; set; } = Guid.Empty;
+    }
+
+    public class QuestionReadDto
+    {
+        public Guid Id { get; set; } = Guid.Empty;
+        public string QuestionText { get; set; } = string.Empty;
+        public string Difficulty { get; set; } = string.Empty;
+
+        public string Status { get; set; } = string.Empty;
+        public AnswerDto[] Answers { get; set; } = new AnswerDto[4]
+        {
+            new(), new(), new(), new()
+        };
+        public QuestionCategoryDto Category { get; set; } = new();
     }
 }
